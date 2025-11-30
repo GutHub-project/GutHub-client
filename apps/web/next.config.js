5d7b4611f295bb9d1890d6346a3a2dad936d79c1
ben <ohamin26@gmail.com>
2025-11-30 14:50:29 +0900

    chore(web): 빌드 시 rn 모듈 포함하지 않도록 수정

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const path = require('path');
+const webpack = require('webpack');
 
 const nextConfig = async () => {
   const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
@@ -17,6 +18,14 @@ const nextConfig = async () => {
         'react-native$': 'react-native-web',
       };
       config.resolve.extensions = ['.web.js', '.web.jsx', '.web.ts', '.web.tsx', ...config.resolve.extensions];
+
+      // @react-native/assets-registry 모듈을 stub으로 대체
+      const stubPath = path.resolve(__dirname, 'webpack-stubs/assets-registry.js');
+      config.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(/^@react-native\/assets-registry\/registry$/, stubPath)
+      );
+      config.plugins.push(new webpack.NormalModuleReplacementPlugin(/^@react-native\/assets-registry$/, stubPath));
+
       return config;
     },
     async redirects() {
