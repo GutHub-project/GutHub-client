9c6c2793d35ecfb4dec2fcc406dbe1fb2ebf0422
ben <ohamin26@gmail.com>
2025-11-30 14:50:29 +0900

    chore(web): 빌드 시 rn 모듈 포함하지 않도록 수정

---
+++ apps/web/webpack-stubs/assets-registry.js
@@ -0,0 +1,17 @@
+export function registerAsset(asset) {
+  return 1;
+}
+
+export function getAssetByID(assetId) {
+  return {
+    __packager_asset: true,
+    fileSystemLocation: '',
+    httpServerLocation: '',
+    width: 0,
+    height: 0,
+    scales: [1],
+    hash: '',
+    name: '',
+    type: 'png',
+  };
+}
