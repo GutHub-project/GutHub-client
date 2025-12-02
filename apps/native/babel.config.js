e652cfd105b3094d89acb3f66666feba94def987
ben <ohamin26@gmail.com>
2025-12-02 12:18:56 +0900

    feat(native): 환경 변수 설정 추가

--- apps/native/babel.config.js
+++ apps/native/babel.config.js
@@ -1,7 +1,36 @@
 module.exports = function (api) {
   api.cache(true);
+  
+  const getEnvPath = () => {
+    const appEnv = process.env.APP_ENV || process.env.NODE_ENV || 'development';
+    
+    if (appEnv === 'production') {
+      return '.env.production';
+    } else if (appEnv === 'development') {
+      return '.env.development';
+    } else {
+      return '.env';
+    }
+  };
+  
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
+    plugins: ["expo-router/babel",
+      [
+        'module:react-native-dotenv',
+        {
+          "envName": "APP_ENV",
+          "moduleName": "@env",
+          "path": getEnvPath(),
+          "blocklist": null,
+          "allowlist": null,
+          "blacklist": null, 
+          "whitelist": null, 
+          "safe": false,
+          "allowUndefined": true,
+          "verbose": false
+        },
+      ],
+    ],
   };
 };
