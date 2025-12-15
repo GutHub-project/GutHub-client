934a4dcbdf90e90e14c4a3ed75c5721a79e43f08
ben <ohamin26@gmail.com>
2025-12-15 17:05:34 +0900

    fix(shared): 앱바 배경 옵션 추가

--- packages/main-feature/src/pages/general/diet/index.tsx
+++ packages/main-feature/src/pages/general/diet/index.tsx
@@ -3,7 +3,7 @@ import Record from "./components/record";
 
 const Diet = () => {
   return (
-    <div className="flex flex-col min-h-[61vh] bg-white rounded-t-[30px] p-[20px] h-full gap-[24px] shadow-[0px_1px_12.6px_0px_rgba(255,45,45,0.25)]">
+    <div className="flex flex-col min-h-[61vh] bg-white rounded-t-[30px] p-[20px] h-full gap-[22px] shadow-[0px_1px_12.6px_0px_rgba(255,45,45,0.25)]">
       <Rate />
       <Record />
     </div>
--- packages/shared/src/components/AppBar/index.tsx
+++ packages/shared/src/components/AppBar/index.tsx
@@ -13,11 +13,12 @@ interface AppBarProps {
   leftContent?: ReactNode;
   text?: string;
   rightContent?: ReactNode;
+  bgColor?: string;
 }
 
-const AppBar = ({ leftContent, text, rightContent }: AppBarProps) => {
+const AppBar = ({ leftContent, text, rightContent, bgColor = 'bg-background' }: AppBarProps) => {
   return (
-    <div className="fixed top-0 left-auto right-auto z-100 flex justify-between items-center border-none pt-4 px-4 pb-2 w-full h-[32px]">
+    <div className={`fixed top-0 left-auto right-auto z-100 flex justify-between items-center border-none pt-4 px-4 pb-2 w-full h-[32px] ${bgColor}`}>
       <div className="flex items-start">
         {leftContent && <div className="mr-6 flex items-center">{leftContent}</div>}
         <span className="font-semibold text-[20px] whitespace-nowrap max-w-full">
