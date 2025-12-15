cb1b9489d7eebb67e483c59fd7f608e9aaee33b1
ben <ohamin26@gmail.com>
2025-12-15 16:56:37 +0900

    feat(main-feature): 초기 메인화면 퍼블리싱

---
+++ packages/main-feature/src/pages/general/dateStatus/index.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { useState } from "react";
+import formattedDate from "./utils/formattedDate";
+
+const DateStatus = () => {
+  const [selectedDate, setSelectedDate] = useState(1);
+  const dataArr = [
+    formattedDate(new Date(Date.now() - 24 * 60 * 60 * 1000)),
+    formattedDate(new Date(Date.now())),
+    formattedDate(new Date(Date.now() + 24 * 60 * 60 * 1000)),
+  ];
+
+  const handleDateClick = (idx: number) => {
+    setSelectedDate(idx);
+  }
+
+  return (
+    <div className="relative mt-[14px] px-[20px]">
+      <div className="flex items-center justify-between">
+        {dataArr.map((item, index) => (
+          <button 
+            key={item} 
+            className={`text-gray-400 text-[14px] font-medium ${selectedDate === index ? "text-main border-b border-main" : ""}`} 
+            onClick={() => handleDateClick(index)}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+      <div className="flex justify-center items-center w-full h-[240px]">
+        <Image src="/AppBar/logo.svg" alt="gut" width={240} height={240} />
+      </div>
+      <div className="w-full h-[38px] bg-Linear text-center text-text flex items-center justify-center rounded-[10px] opacity-100 gap-[3px]">
+        <span className="text-[14px] text-text font-medium">내 장 상태 :</span>
+        <span className="text-[14px] text-main font-medium">아파요..</span>
+      </div>
+    </div>
+  )
+}
+
+export default DateStatus;
--- packages/main-feature/src/pages/general/index/components/Layout.tsx
+++ packages/main-feature/src/pages/general/index/components/Layout.tsx
@@ -2,10 +2,10 @@ import { AppBar, Layout as SharedLayout } from "@repo/shared";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="min-h-screen w-full relative overflow-hidden bg-[#FFF5F5]">
       <AppBar leftContent={<AppBar.MainLogo nickName="허브" />} rightContent={<AppBar.Date date="4일" />} />
       <SharedLayout>{children}</SharedLayout>
-    </>
+    </div>
   )
 }
 
