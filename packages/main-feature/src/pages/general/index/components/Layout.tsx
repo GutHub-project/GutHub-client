5357166a4453df5ea398a36fb855a05c7b8ca726
ben <ohamin26@gmail.com>
2025-12-15 17:05:34 +0900

    fix(shared): 앱바 배경 옵션 추가

--- packages/main-feature/src/pages/general/index/components/Layout.tsx
+++ packages/main-feature/src/pages/general/index/components/Layout.tsx
@@ -3,7 +3,7 @@ import { AppBar, Layout as SharedLayout } from "@repo/shared";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen w-full relative overflow-hidden bg-[#FFF5F5]">
-      <AppBar leftContent={<AppBar.MainLogo nickName="허브" />} rightContent={<AppBar.Date date="4일" />} />
+      <AppBar leftContent={<AppBar.MainLogo nickName="허브" />} rightContent={<AppBar.Date date="4일" />} bgColor="bg-[#FFF5F5]" />
       <SharedLayout>{children}</SharedLayout>
     </div>
   )
