7e8f226851b74c1e983aaa5b60e856f86d10835b
jin <ha983885@snu.ac.kr>
2026-01-11 21:35:33 +0900

    fix: explicitly export all components from main-feature index

--- packages/main-feature/src/index.ts
+++ packages/main-feature/src/index.ts
@@ -1,4 +1,16 @@
-export * from './pages';
-export * from './components/web';
-export * from './components/shopping';
+// Pages
+export { default as General } from './pages/general/index/index';
+
+// Web Components
+export { ShoppingPage } from './components/web/ShoppingPage';
+export { RecordPage } from './components/web/RecordPage';
+
+// Shopping Components
+export { ShoppingScreen } from './components/shopping/ShoppingScreen';
+export { ProductDetailPage } from './components/shopping/ProductDetailPage';
+export { ReviewWritePage } from './components/shopping/ReviewWritePage';
+export { ReviewListPage } from './components/shopping/ReviewListPage';
+export { ReviewDetailPage } from './components/shopping/ReviewDetailPage';
+export { ReviewFilterModal } from './components/shopping/ReviewFilterModal';
+export * from './components/shopping/types';
 
